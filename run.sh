61061fa1f7d9ada53bfa5bc25ebf44f1950f09a1
Heshy Roskes <jroskes1@jhu.edu>
2015-09-18 16:49:31 +0200

    Use trees from Ulascan

--- run.sh
+++ run.sh
@@ -3,13 +3,16 @@
 cd /afs/cern.ch/user/h/hroskes/work/public/Run2MC/ZZ4lanalyticalfits
 
 for a in $(ls .. | grep "^VBF\|^HJJ"); do
-    files="$(ls ../$a/ | grep '\.root$')"
+  aa=$(echo $a | sed "s/pseudoscalar/_PS/" | sed "s/scalar/_SM/" | sed "s/fa3/_fa3/" | sed "s/phi/_phi/")
+  echo $aa
+  for b in 2e2mu 4e 4mu; do
+    files="$(ls ../CMSJHU_AnalysisMacros/JHUSpinWidthPaper_2015/LHEAnalyzer/$aa/ | grep $b'\.root$')"
     if ! [ "$files" ]; then
         continue
     fi
     nfiles=$(echo "$files" | wc -w)
 
-    filename=${a/./}
+    filename=${a/./}_$b
 
     g1=$(grep "ghz1 = " ../$a/JHUGen/JHUGenerator/mod_Parameters.F90 | sed "s/.*(//" | sed "s/,.*//" | sed "s/d/e/")
     Re_g4=$(grep "ghz4 = " ../$a/JHUGen/JHUGenerator/mod_Parameters.F90 | sed "s/.*(//" | sed "s/,.*//" | sed "s/d/e/")
@@ -31,25 +34,24 @@ for a in $(ls .. | grep "^VBF\|^HJJ"); do
     comma=
     for file in $files; do
         echo -n "$comma"'
-        "'../$a/$file'"' >> $filename.C
+        "'../CMSJHU_AnalysisMacros/JHUSpinWidthPaper_2015/LHEAnalyzer/$aa/$file'"' >> $filename.C
         comma=,
     done
 
     echo '
     };
 
-    angularDistributions_spin0(nfiles, files, "'$a'"'", $g1, $Re_g4, $Im_g4);
+    angularDistributions_spin0(nfiles, files, "'$a/$b'"'", $g1, $Re_g4, $Im_g4);
 }" >> $filename.C
 
 
-    #if ( echo $a | grep -q "\." ); then
+    if ( echo $a | grep -q "HJJ" ); then
         echo "
             cd /afs/cern.ch/work/h/hroskes/public/Run2MC/CMSSW_7_4_1 && eval "'$(scram ru -sh)'" && cd /afs/cern.ch/user/h/hroskes/work/public/Run2MC/ZZ4lanalyticalfits &&
             echo '
             .x loadLib.C
             .x $filename.C+
-        ' | root -l -b" | bsub -q 1nd -J plot$a
-    #fi
-
-
+        ' | root -l -b" | bsub -q 1nd -J plot$a$b
+    fi
+  done
 done
